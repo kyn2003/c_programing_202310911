b090596f6b95ac3b69e7de37687ce79eebe13013
Kim Yoona <127467055+kyn2003@users.noreply.github.com>
2024-09-24 00:25:09 +0900

    Create hw2.c

---
+++ hw2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double kilo;
+    double mile;
+    printf("Please enter kilometers: ");
+    scanf("%lf", &kilo);
+    mile = kilo / (1.609);
+    printf("%.1f km is equal to %.1f miles.\n", kilo, mile);
+    return 0;
+}
